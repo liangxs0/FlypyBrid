2cb2b43a96c4375f2045f91d181034e65b968a66
liangxs0 <1033808656@qq.com>
2018-03-29 09:42:44 +0800

    new cerate

---
+++ Objects/main.d
@@ -0,0 +1,5 @@
+.\objects\main.o: app\main.c
+.\objects\main.o: .\system\stm32f10x_map.h
+.\objects\main.o: .\system\stm32f10x_conf.h
+.\objects\main.o: .\system\stm32f10x_type.h
+.\objects\main.o: .\system\cortexm3_macro.h
---
+++ app/main.c
@@ -0,0 +1,13 @@
+/*
+* Project: FlypyBrid
+*	Author : Romas
+*	Time:	2018-3-29
+*/
+#include "stm32f10x_map.h"
+
+int main()
+{
+		while(1);
+		
+}
+
